2b9a8454fceb031507eceee0fa34da87507eece6
Torsha Mazumdar <torshamazumdar@gmail.com>
2021-11-13 12:19:29 -0800

    modified signature of reserve

--- ParkingLot.h
+++ ParkingLot.h
@@ -7,7 +7,7 @@ using namespace std;
 class ParkingLot
 {
 public:
-	virtual void reserve() = 0;
+	virtual bool reserve(time_t startTime, time_t endTime) = 0;
 	virtual string getLotName() = 0;
 	virtual bool checkIsLotFull() = 0;
 
